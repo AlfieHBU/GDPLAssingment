bf1ecd67573b87b334aece26f016cddff064aeda
Alfie Harrington <s5712312@bournemouth.ac.uk>
2025-05-05 13:15:37 +0100

    Push To Uni 5/5/25
    bruh

--- Assets/Scripts/ProjectilePrefab.cs
+++ Assets/Scripts/ProjectilePrefab.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProjectilePrefab : MonoBehaviour
 {
     public float Yfall = 0f;
+    public ProjectileBehaviour projectilebehaviour;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
+
+            int remaining = GameObject.FindGameObjectsWithTag("Enemy").Length - 1;
+
+            FindObjectOfType<UIUpdate>().UpdateTargetCount(remaining);
         }
-        
+
 
         if (collision.gameObject.CompareTag("Ammo_Pickup"))
         {
             Destroy(collision.gameObject);
         }
         Destroy(gameObject);
-
+    }
+ 
+    private void Update()
+    {
+        if (transform.position.y < Yfall)
         {
-            if (transform.position.y < Yfall)
-                Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
+
 }
---
+++ Assets/Scripts/UIUpdate.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class UIUpdate : MonoBehaviour
+{
+    public TextMeshProUGUI targetsLeftText;
+    public TextMeshProUGUI powerText;
+    public TextMeshProUGUI yText;
+    public TextMeshProUGUI xText;
+
+
+    void Start()
+    {
+        int initialTargets = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        UpdateTargetCount(initialTargets);
+    }
+
+    public void UpdateTargetCount(int count)
+    {
+        targetsLeftText.text = "TARGETS LEFT: " +  count;
+    }
+
+    public void UpdatePower(float power) 
+    {
+        powerText.text = "POWER: " + Mathf.RoundToInt(power);
+    }
+
+    public void UpdateAngles(float yaw, float pitch)
+    {
+        yText.text = "ELEVATION ANGLE: " + Mathf.RoundToInt(pitch) + "°";
+        xText.text = "HORIZONTAL ANGLE: " + Mathf.RoundToInt(yaw) + "°";
+    }
+}
+
+ 
